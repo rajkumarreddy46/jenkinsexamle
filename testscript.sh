9e6bcc80221d8979556e3b69f120b54c3cc01f0b
rajkumarreddy <rajkumarreddy46@gmail.com>
2020-04-10 21:50:02 +0530

    trying for last time

--- testscript.sh
+++ testscript.sh
@@ -2,4 +2,4 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "edited file and removed old file"
+echo "edited file and removed old file and trying for last time"
